86510e37de7e021c368c99f578c64b84d05cbf6e
darkrho <darkrho@outlook.es>
2024-05-24 09:35:39 +0200

    adding mongose validators to the schema

--- index.js
+++ index.js
@@ -53,14 +53,13 @@ app.delete("/api/notes/:id", (request, response, next) => {
 
 /* update the note */
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -68,7 +67,7 @@ app.put("/api/notes/:id", (request, response, next) => {
 });
 
 /* add a note  */
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
   if (!body.content === undefined) {
     return response.status(400).json({ error: "content missing" });
@@ -77,9 +76,12 @@ app.post("/api/notes", (request, response) => {
     content: body.content,
     important: body.important || false,
   });
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 /* middleware error handler */
@@ -87,7 +89,10 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
+  next(error);
 };
 app.use(errorHandler);
 
